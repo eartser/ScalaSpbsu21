1f52d5113ce8a6985f8aaf1d79ce84eb4d035635
Elizaveta Artser <eartser19@gmail.con>
2021-03-07 11:56:01 +0300

    implement generic list

--- 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
+++ 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
@@ -2,19 +2,53 @@ package org.spbsu.mkn.scala
 
 import org.spbsu.mkn.scala.MyGenericList._
 
-sealed trait MyGenericList {
-  def head: Int
-  def tail: MyGenericList
-  def drop(n: Int): MyGenericList
-  def take(n: Int): MyGenericList
-  def map(f: Int => Int): MyGenericList
-  def ::(elem: Int): MyGenericList = ???
+sealed trait MyGenericList[+T] {
+  def head: T
+  def tail: MyGenericList[T]
+  def drop(n: Int): MyGenericList[T]
+  def take(n: Int): MyGenericList[T]
+  def map[NT](f: T => NT): MyGenericList[NT]
+  def ::[ST >: T](elem: ST): MyGenericList[ST] = new ::(elem, this)
+}
+
+case class ::[T](head: T, tail: MyGenericList[T]) extends MyGenericList[T] {
+  override def drop(n: Int): MyGenericList[T] = {
+    if (n < 0) undef
+    n match {
+      case 0 => this
+      case _ => tail.drop(n - 1)
+    }
+  }
+  override def take(n: Int): MyGenericList[T] = {
+    if (n < 0) undef
+    n match {
+      case 0 => MyNil
+      case _ => head :: tail.take(n - 1)
+    }
+  }
+  override def map[NT](f: T => NT): MyGenericList[NT] = f(head) :: tail.map(f)
+}
+
+case object MyNil extends MyGenericList[Nothing] {
+  override def head: Nothing = undef
+  override def tail: MyGenericList[Nothing] = MyNil
+  override def drop(n: Int): MyGenericList[Nothing] = if (n == 0) MyNil else undef
+  override def take(n: Int): MyGenericList[Nothing] = if (n == 0) MyNil else undef
+  override def map[NT](f: Nothing => NT): MyGenericList[Nothing] = MyNil
 }
 
 object MyGenericList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): MyGenericList = ???
-  def size(intList: MyGenericList): Int     = ???
+  def fromSeq[T](seq: Seq[T]): MyGenericList[T] = if (seq.isEmpty) MyNil else seq.head :: fromSeq(seq.drop(1))
+  def sum(myGenericList: MyGenericList[Int]): Int      = myGenericList match {
+    case MyNil => undef
+    case x :: MyNil => x
+    case x :: xs => x + sum(xs)
+  }
+  def size[T](myGenericList: MyGenericList[T]): Int     = myGenericList match {
+    case MyNil => 0
+    case _ :: xs => 1 + size(xs)
+  }
   // extra task: implement sum using foldLeft
   // def foldLeft(???)(???): ??? = ???
 }
--- 03/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
+++ 03/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
@@ -1,12 +1,11 @@
 package org.spbsu.mkn.scala
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.spbsu.mkn.scala
 import org.spbsu.mkn.scala.MyGenericList.{fromSeq, size, sum}
 
 class MyGenericListTest extends AnyFunSuite {
 
-  // remove after implementing actual MyNil
-  object MyNil
 
   test("head") {
     assert(fromSeq(Seq(1,2,3)).head == 1)
@@ -34,7 +33,7 @@ class MyGenericListTest extends AnyFunSuite {
   }
 
   test("map") {
-    assert(MyNil.map(_ * 2) == MyNil)
+    assert((MyNil : MyGenericList[Int]).map(_ * 2) == MyNil)
     assert(fromSeq(Seq(1,2,3)).map(_ * 2) == fromSeq(Seq(2,4,6)))
     assert(fromSeq(Seq(1,2,3)).map(identity) == fromSeq(Seq(1,2,3)))
   }
